e896f811224c02ae04d69a72c6315b9734407718
Louis <215416321@tut4life.ac.za>
2022-05-26 09:07:15 +0200

    two items

---
+++ routes/two_items.js
@@ -0,0 +1,34 @@
+const express =require('express');
+const router = express.Router();
+const app=express();
+module.exports = router;
+
+const database=require('./database');
+
+// get single data
+
+router.get('/record/:User_id',(req,res)=>{
+    let gID = req.params.User_id;
+    
+    let qr = `select Form_check, Tempareture, Date from record where User_id = '${gID}'`;
+    
+    database.query(qr,(err,result)=>{
+    
+      if(err){console.log(err);
+        console.log(result,'result')
+        res.send({message:'Unsuccessful'});
+        } 
+        else{
+            res.send({
+                message:'Successful',
+                data:result
+            });
+    
+        }
+            
+    }); 
+       
+      
+    
+
+  });
